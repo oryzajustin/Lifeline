4e5c73413419744a771da6ca1e2fcc8b32d6ef12
YuanC <jerrych.97@gmail.com>
2019-11-23 17:20:47 -0500

    finished backend

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,80 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Asserting that all environment variables are there:
+require('dotenv').config()
+console.log("\n--------------------------------------------------------------------------------");
+console.log("ENVIRONMENT VARIABLES");
+console.log("Twilio Account Id: " + process.env.TWILIO_ACC_ID);
+console.log("Twilio Account Id: " + process.env.TWILIO_ACC_ID);
+console.log("Twilio Auth Token: " + process.env.TWILIO_AUTH_TOKEN);
+console.log("Twilio Phone #: " + process.env.TWILIO_PHONE);
+console.log("Demo Phone #: " + process.env.DEMO_PHONE);
+console.log("Twilio Voice Url: " + process.env.VOICE_URL);
+console.log("--------------------------------------------------------------------------------\n");
 
-var app = express();
+var twilio = require('twilio');
+var twilio_client = new twilio(process.env.TWILIO_ACC_ID, process.env.TWILIO_AUTH_TOKEN);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var port = process.env.PORT || 3000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html'); 
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var distressSignals = [];
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+io.on('connection', function(socket){
+  console.log("New Connexxion: " + socket.id);
+  socket.emit('UpdateDistressSignals', distressSignals);
+
+  // Add distress signal to the list and refreshes clients
+  socket.on('CreateDistressSignal', function(data){
+    distressSignals.push({
+      id: socket.id, 
+      coords: data.coords,
+      time: Date.now()
+    });
+
+    twilio_client.calls.create({
+      url: process.env.VOICE_URL,
+      to: process.env.DEMO_PHONE, // Text this number
+      from: process.env.TWILIO // From a valid Twilio number
+    })
+    .then((call) => console.log(call.sid));
+
+    // Update all other users
+    socket.emit('UpdateDistressSignals', distressSignals);
+    socket.broadcast.emit('UpdateDistressSignals', distressSignals);
+  });
+
+  // Delete Distress Signal by ID and refreshes clients
+  socket.on('ResolveDistressSignal', function(signal_id){
+    // Update the rest of the app with the new distress signal list
+    let index = -1;
+
+    for(i = 0; i < distressSignals.length; i++) {
+      if (distressSignals[i]['id'] === signal_id) {
+        index = i;
+        break;
+      }
+    }
+
+    // If found. Otherwise, shame on you, Justin.
+    if (index > -1) {
+      distressSignals.splice(index);
+      socket.emit('UpdateDistressSignals', distressSignals);
+      socket.broadcast.emit('UpdateDistressSignals', distressSignals);
+    }
+  });
+
+  // Refreshes the client
+  socket.on('UpdateDistressSignals', function(){
+    socket.emit('UpdateDistressSignals', distressSignals);
+  });
 });
 
-module.exports = app;
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
