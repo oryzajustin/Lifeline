abce9a9dd5a2224427a5fc8b854ad348edd001fc
YuanC <jerrych.97@gmail.com>
2019-11-24 08:46:47 -0500

    please

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res){
 });
 
 var distressSignals = [];
-var enableTwilio = true;
+var enableTwilio = false;
 var connectCounter = 0; 
 
 io.on('connection', function(socket){
